db115c61d8f4ab144d61fa0b9445894c278df6f8
Brian R <braab86@gmail.com>
2022-07-27 11:05:43 -0400

    readme updated

--- csv_test/csv_test.py
+++ csv_test/csv_test.py
@@ -1,11 +1,11 @@
 from helpers import get_csv_data, get_pivot_df, get_alphabet
 
 
-list_url = get_alphabet()
+url_list = get_alphabet()
 root_url = 'https://public.wiwdata.com/engineering-challenge/data/'
 
 
-df = get_pivot_df(get_csv_data(root_url, list_url), 'length',
+df = get_pivot_df(get_csv_data(root_url, url_list), 'length',
                   'user_id', 'path', 'sum')
 
 try:
